b0a1a0172d5190ae2999a0b0977ab149426d6ce9
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-03 11:02:36 +0100

    Create Burger components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 
+import classes from './Layout.module.css';
+
 const Layout = props => (
   <Fragment>
     <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Fragment>
