0cdf6149caf1f086b5b58a9c4f81bae2170aba26
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-04 12:27:06 +0100

    Create Burger components (dynamic passing ingredients)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,9 +5,13 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
   const transformedIngredients = Object.keys(props.ingredients)
-    .map(
+    .map(ing => {
+      return [...Array(props.ingredients[ing])].map((_, i) => {
+        return <BurgerIngredient key={ing + 1} type={ing}/>
       })
-    });
+    })
+
+
 
   return (
     <div className={classes.Burger}>
