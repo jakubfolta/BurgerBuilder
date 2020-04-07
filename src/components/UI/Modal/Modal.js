e4ce8c5f717188b1190cee6bdc3bdbcc6f9b67bc
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-07 13:10:12 +0100

    Create orderSummary modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,7 +5,7 @@ import classes from './Modal.module.css';
 const Modal = props => (
   <div
     className={classes.Modal}
-    style={{visibility: props.show ? 'visible' : 'hidden', opacity: props.show ? 1 : 0}} >
+    style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? 1 : 0 }} >
     {props.children}
   </div>
 )
