47538f60a087a1c6de3d9eee5d1089eb5d7bb86f
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-08 10:38:31 +0100

    Create Toolbar component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Logo.module.css';
+import burgerLogo from '../../assets/images/original.png';
+
+const Logo = props => (
+  <div className={classes.Logo}>
+    <img src={burgerLogo} alt="logo"/>
+  </div>
+)
+
+export default Logo;
