0f3c5a53e3906829ab80621d307c539633b639e9
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-09 09:42:36 +0100

    Create drawer toggle button

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -4,8 +4,10 @@ import classes from './DrawerToggle.module.css';
 
 const DrawerToggle = props => (
   <div
-    className={classes.DrawerToggle}
-    onClick={props.show} />
+    className={classes.Toggle}
+    onClick={props.show} >
+    <span className={classes.Toggle__button} />
+  </div>
 )
 
 export default DrawerToggle;
