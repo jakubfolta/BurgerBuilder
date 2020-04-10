3ab1d90616faa6f84b26fb8790828bb1b0af27fe
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-10 11:13:39 +0100

    Add PropTypes to component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import PropTypes from 'prop-types';
 
 const Burger = props => {
   let transformedIngredients = Object.keys(props.ingredients)
@@ -25,6 +26,10 @@ const Burger = props => {
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
+}
+
+Burger.propTypes = {
+  ingredients: PropTypes.object.isRequired
 }
 
 export default Burger;
