160b6df101901f3c6ab38b6afab9d29445385830
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-10 10:21:10 +0100

    Add PropTypes to component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,32 +4,33 @@ import Button from '../../UI/Button/Button';
 import PropTypes from 'prop-types';
 
 class OrderSummary extends Component {
-  ingredientSummary = Object.keys(this.props.state.ingredients)
-    .map(el => {
-      return (
-        <li key={el}>
-          <span style={{textTransform: 'capitalize'}}>{el}</span>: {this.props.state.ingredients[el]}
-        </li>
-      )
-    })
   render() {
+    const ingredientSummary = Object.keys(this.props.state.ingredients)
+      .map(el => {
+        return (
+          <li key={el}>
+            <span style={{textTransform: 'capitalize'}}>{el}</span>: {this.props.state.ingredients[el]}
+          </li>
+        )
+      })
+
     return (
       <Fragment>
         <h3>Your order</h3>
         <p>A delicious burger with following ingredients:</p>
         <ul>
-          {this.ingredientSummary}
+          {ingredientSummary}
         </ul>
         <p><strong>Price: {(this.props.state.totalPrice).toFixed(2)}</strong></p>
         <p>Continue to checkout?</p>
         <Button
           btnType="Danger"
           clicked={this.props.purchaseCancelled} >Cancel</Button>
-          <Button
-            btnType="Success"
-            clicked={this.props.purchaseContinued} >Continue</Button>
-          </Fragment>
-        )
+        <Button
+          btnType="Success"
+          clicked={this.props.purchaseContinued} >Continue</Button>
+      </Fragment>
+    );
   }
 }
 
