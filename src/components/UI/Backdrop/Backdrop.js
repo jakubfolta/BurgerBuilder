8ef423b23470ebfe97a8baf2475b228c44c56334
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-10 11:46:00 +0100

    Add PropTypes to component

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import classes from './Backdrop.module.css';
+import PropTypes from 'prop-types';
 
 const Backdrop = props => (
   props.show ? <div className={classes.Backdrop} onClick={props.hide}></div> : null
 );
+
+Backdrop.propTypes = {
+  show: PropTypes.bool.isRequired,
+  hide: PropTypes.func.isRequired
+}
 
 export default Backdrop;
