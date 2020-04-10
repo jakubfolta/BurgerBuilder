66df3e5f100ef1abab34996f45e225baaf694907
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-10 11:18:23 +0100

    Add PropTypes to component

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import classes from './NavigationItem.module.css';
+import PropTypes from 'prop-types';
 
 const NavigationItem = props => (
   <li className={classes.Item} >
@@ -9,5 +10,9 @@ const NavigationItem = props => (
       className={props.active ? classes.active : null} >{props.children}</a>
   </li>
 );
+
+NavigationItem.propTypes = {
+  link: PropTypes.string.isRequired
+}
 
 export default NavigationItem;
