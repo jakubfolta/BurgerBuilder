7b1b6eec9813ae1ac3a8fd69978fb424d0c109f8
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-10 12:04:16 +0100

    Improving performance componentShouldUpdate()

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import Button from '../../UI/Button/Button';
 import PropTypes from 'prop-types';
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.state.ingredients)
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('[OrderSummary] will update');
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.state.ingredients)
     .map(el => {
       return (
         <li key={el}>
-          <span style={{textTransform: 'capitalize'}}>{el}</span>: {props.state.ingredients[el]}
+          <span style={{textTransform: 'capitalize'}}>{el}</span>: {this.props.state.ingredients[el]}
         </li>
       )
     })
 
-  return (
-    <Fragment>
-      <h3>Your order</h3>
-      <p>A delicious burger with following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Price: {(props.state.totalPrice).toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button
-        btnType="Danger"
-        clicked={props.purchaseCancelled} >Cancel</Button>
-      <Button
-        btnType="Success"
-        clicked={props.purchaseContinued} >Continue</Button>
-    </Fragment>
+    return (
+      <Fragment>
+        <h3>Your order</h3>
+        <p>A delicious burger with following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Price: {(this.props.state.totalPrice).toFixed(2)}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button
+          btnType="Danger"
+          clicked={this.props.purchaseCancelled} >Cancel</Button>
+        <Button
+          btnType="Success"
+          clicked={this.props.purchaseContinued} >Continue</Button>
+      </Fragment>
     );
-
+  }
 }
 
 OrderSummary.propTypes = {
