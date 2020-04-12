b4ea5fd47715fcce538f7e53ee47ed6803b9f25b
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-12 21:59:46 +0100

    Improving performance componentShouldUpdate()

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,14 +5,12 @@ import Backdrop from '../Backdrop/Backdrop';
 import PropTypes from 'prop-types';
 
 class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show
-  }
+  
 
   componentDidUpdate() {
     console.log('[Modal] will update');
   }
-  
+
   render() {
     return (
       <Fragment>
