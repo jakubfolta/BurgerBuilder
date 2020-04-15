2a6dfbcfcf0d2c89ca25194b21eb05e6e22b5617
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-15 09:06:55 +0100

    Handle error - Accessing server

--- src/containers/withErrorHandler/withErrorHandler.js
+++ src/containers/withErrorHandler/withErrorHandler.js
@@ -14,7 +14,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
         return req;
       });
 
-      axios.interceptors.response.use(null, error => {
+      axios.interceptors.response.use(res => res, error => {
         this.setState({error: error});
       });
     }
