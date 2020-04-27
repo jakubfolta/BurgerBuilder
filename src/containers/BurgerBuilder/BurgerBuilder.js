d73696a85d651e601a7efd8e37e83b4c146c0715
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-27 09:04:45 +0100

    Pass query params - solidifying

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -107,15 +107,7 @@ class BurgerBuilder extends Component {
     //   .catch(error => {
     //     this.setState({loading: false, order: false})
     //   })
-    const queryParams = [];
-    for (let i in this.state.ingredients) {
-      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
-    }
-
-    this.props.history.push({
-      pathname: '/checkout',
-      search:'?' + queryParams.join('&')
-    })
+    
   };
 
   render() {
