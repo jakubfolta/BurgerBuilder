6094c56b45a7cd277a33b47d9abb4973447e1cb1
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-07 12:22:47 +0100

    Set up reducer, store...

---
+++ src/store/actions.js
@@ -0,0 +1,18 @@
+import * as actionTypes from './reducer.js';
+
+const initialState = {
+  ingredients: '',
+  totalPrice: null
+}
+
+const reducer = (state = initialState, action) => {
+  // switch (action.type) {
+  //   case actionTypes.:
+  //     return {
+  //       ...state
+  //     }
+  // }
+  return state;
+}
+
+export default reducer;
