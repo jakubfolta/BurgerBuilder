448824ba9305123553a0d889493e8c018e149151
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-14 09:37:10 +0100

    Implement advanced redux - add order actions

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,6 +1,21 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
 
+export const PURCHASE_BURGER_SUCCESS = (id, orderData) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    orderId: id,
+    orderData: orderData
+  };
+};
+
+export const purchaseBurgerFail = (error) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAIL,
+    error: error
+  };
+};
+
 
 
 export const sendOrder = (order) => {
@@ -8,3 +23,13 @@ export const sendOrder = (order) => {
     type: actionTypes.SEND_ORDER
   }
 }
+
+
+axios.post('/orders.json', order)
+  .then(response => {
+    this.setState({loading: false})
+    this.props.history.push('/');
+  })
+  .catch(error => {
+    this.setState({loading: false})
+  });
