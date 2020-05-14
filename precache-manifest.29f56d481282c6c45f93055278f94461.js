c64841a1b0b23276f0960cb438950ae2a7419a23
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-14 22:56:16 +0100

    Updates

---
+++ precache-manifest.29f56d481282c6c45f93055278f94461.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7f8a8a89259b69a1a2ff64d213b737",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "db08b3e74e90e8f2cff7",
+    "url": "/BurgerBuilder/static/css/main.45aa7e70.chunk.css"
+  },
+  {
+    "revision": "8ab6c7310170deef0bc2",
+    "url": "/BurgerBuilder/static/js/2.11afabc3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerBuilder/static/js/2.11afabc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db08b3e74e90e8f2cff7",
+    "url": "/BurgerBuilder/static/js/main.43d7f413.chunk.js"
+  },
+  {
+    "revision": "994a3903c5ff2923d806",
+    "url": "/BurgerBuilder/static/js/runtime-main.603c822a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/original.b8503d26.png"
+  }
+]);
