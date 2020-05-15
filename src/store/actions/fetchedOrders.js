bc2c323237ed41bfc0a717549c252b770f2d75b6
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-15 09:32:51 +0100

    Implement advanced redux - fetching orders via Redux

--- src/store/actions/fetchedOrders.js
+++ src/store/actions/fetchedOrders.js
@@ -1,4 +1,23 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
 
-export const fetchOrders = () =>
+export const fetchOrders = () => {
+  return dispatch => {
+    axios.get('orders.json')
+    .then(res => {
+      dispatch(fetchOrdersSuccess())
+    })
+  }
+}
+
+export const fetchOrdersSuccess = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS
+  }
+}
+
+export const fetchOrdersFail = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL
+  }
+}
