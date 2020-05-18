5170b0bd8d088e0887ce4db3fedbf4be732b3cb7
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-18 13:07:19 +0100

    Redux advanced - refactoring

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -24,34 +24,41 @@ const purchaseSuccess = (state, action) => {
   });
 };
 
+const purchaseFail = (state, action) => {
+  return updateObject(state, {loading: false});
+};
+
+const purchaseInit = (state, action) => {
+  return updateObject(state, {purchased: false});
+};
+
+const fetchStart = (state, action) => {
+    return updateObject(state, {loading: true});
+};
+
+const fetchSuccess = (state, action) => {
+  return updateObject(state, {
+    loading: false,
+    orders: action.orders
+  });
+};
+
+const fetchFail = (state, action) => {
+  return updateObject(state, {loading: false});
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     // ORDERS
     case actionTypes.PURCHASE_BURGER_START: return purchaseStart(state, action);
-
-
     case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseSuccess(state, action);
-
-
-    case actionTypes.PURCHASE_BURGER_FAIL:
-      return updateObject(state, {loading: false})
-
-    case actionTypes.PURCHASE_INIT:
-      return updateObject(state, {purchased: false})
+    case actionTypes.PURCHASE_BURGER_FAIL: return purchaseFail(state, action);
+    case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
 
     // FETCHED ORDERS
-    case actionTypes.FETCH_ORDERS_START:
-      return updateObject(state, {loading: true})
-
-    case actionTypes.FETCH_ORDERS_SUCCESS:
-      return updateObject(state, {
-        loading: false,
-        orders: action.orders
-      })
-
-    case actionTypes.FETCH_ORDERS_FAIL:
-      return updateObject(state, {loading: false})
-
+    case actionTypes.FETCH_ORDERS_START: return fetchStart(state, action);
+    case actionTypes.FETCH_ORDERS_SUCCESS: return fetchSuccess(state, action);
+    case actionTypes.FETCH_ORDERS_FAIL: return fetchFail(state, action);
     default:
       return state;
   };
