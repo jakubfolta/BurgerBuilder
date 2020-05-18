e4bc1e37df2b50dec0f96e7ebf7a78c8ce10d230
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-18 09:55:06 +0100

    Redux advanced - refactoring

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -17,11 +17,14 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      return updateObject(state, {ingredients: {
-        ...state.ingredients,
-        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-      },
-      totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]})
+      const updatedIngredient = {[action.ingredientName]: state.ingredients[action.ingredientName] + 1};
+      const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+      const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+      };
+
+      return updateObject(state, updatedState);
 
     case actionTypes.REMOVE_INGREDIENT:
       return updateObject(state, {ingredients: {
