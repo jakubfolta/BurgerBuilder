af9429d885818c341844c15500b8ef422a26c5ce
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-19 14:03:00 +0100

    Redux Authentication

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -2,14 +2,33 @@ import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../utility';
 
 const initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false
+}
 
+const authStart = (state, action) => {
+  return updateObject(state, {error: null, loading: true});
+};
+
+const authSuccess = (state, action) => {
+  return updateObject(state, {
+    token: action.authData.idToken,
+    userId: action.authData.email,
+    error: null,
+    loading: false
+  })
 }
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
-    case actionTypes.AUTH_START:
+  switch (action.type) {
+    case actionTypes.AUTH_START: return authStart(state, action);
 
-      return updateObject(state, {loading: true});
+    case actionTypes.AUTH_SUCCESS:
+      return updateObject(state, {loading: false});
+    case actionTypes.AUTH_FAIL:
+      return updateObject(state, {loading: false});
 
 
     default: return state;
