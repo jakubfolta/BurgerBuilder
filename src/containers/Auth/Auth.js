b11b1bf316b6d74e282c831a7c95e3c22176fc4d
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-20 10:22:36 +0100

    Redux Authentication - add spinner

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import * as authActions from '../../store/actions/index';
 
 class Auth extends Component {
