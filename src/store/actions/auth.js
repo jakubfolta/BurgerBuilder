841f157f9c5f545cc67488a9a8a053c936676ecd
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-24 19:41:30 +0100

    Redux Authentication - authState with local storage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -80,7 +80,9 @@ export const checkAuthState = () => {
     if (!token) {
       dispatch(logout());
     } else {
-      
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      const userId = localStorage.getItem('id');
+      const timeToExpire = expirationDate.getSeconds() - new Date().getSeconds();
       if (expirationDate > new Date()) {
         dispatch(authSuccess(token, userId));
         dispatch(checkAuthTimeout(timeToExpire));
